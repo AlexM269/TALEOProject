40585fca54550f10b23a860d3407512804254dc8
AlexM269 <alexandre.mahjoub@gmail.com>
2023-04-12 15:16:13 +0200

    Création de prototype + implémentation de l'évaluation de similarité + début de passage en .rel

---
+++ recherche.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def indexation_query():
+    print("ouais")
+def preprocess_query():
+    print("preprocess_query")
+def similarity_evaluation(req,doc,seuil):
+    liste = []
+    for i in range(0,len(req)):
+        for j in range(0,len(doc)):
+            prod_vectors = (np.dot(doc[j], req[i])) / (np.linalg.norm(doc[j]) * np.linalg.norm(req[i]))
+            if prod_vectors > seuil:
+                liste.append((i,j,prod_vectors))
+    return liste
+
+def liste_to_doc(data,filename):
+    line=""
+    for i in range(0,len(data)):
+        line = data[i][0]+"  "+data[i][1]+"  "+data[i][2]+"\n"
+    
+        
