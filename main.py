4e145e83d03bed971a2f7404cd51e2d0cac2f04b
AlexM269 <alexandre.mahjoub@gmail.com>
2023-05-03 14:53:08 +0200

    suite tests

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import recherche
-import indexation
+#import indexation
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    req = ((1,0,1,0.5),(0,0.5,0,1))
+    req = ((2,2,2,2),(0,0.5,0,1))
     doc = ((1,0.25,0.90,0.6),(0.2,0.6,0.1,0.9))
-    print(recherche.similarity_evaluation(req, doc, 0.5))
+    print(recherche.similarity_evaluation(req, doc, 0.85))
+    recherche.liste_to_fichier(recherche.similarity_evaluation(req, doc, 0.85))
 
--- recherche.py
+++ recherche.py
@@ -12,17 +12,18 @@ def similarity_evaluation(req,doc,seuil):
         for j in range(0,len(doc)):
             #produit des vecteurs de poids de doc et req sur le produit de leur norme
             prod_vectors = (np.dot(doc[j], req[i])) / (np.linalg.norm(doc[j]) * np.linalg.norm(req[i]))
-            print(i, j, prod_vectors)
             if prod_vectors > seuil:
                 liste.append((i, j, prod_vectors))
     return liste
 
 #A partir de la liste de similarité, crée un fichier de type CISI_dev.REL pour afficher les documents correspondants à la requète
 def liste_to_fichier(data):
-    line = ""
+
     with open("reponse_requete.txt","w") as f:
         for i in range(0,len(data)):
-            line = line + data[i][0]+"  "+data[i][1]+"  "+data[i][2]+"\n"
+            line = ""
+            line = line + str(data[i][0])+"  "+str(data[i][1])+"  "+str(data[i][2])+"\n"
             f.write(line)
+
     
         
